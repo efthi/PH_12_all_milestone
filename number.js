76f20503318ea7647f1e1b1ecd929038c58ec58f
Md Eftequarul Alam <efthaqur@gmail.com>
2025-07-27 23:53:04 +0600

    number concept cleared

---
+++ number.js
@@ -0,0 +1,18 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = '26';
+
+console.log(orangePrice + parseInt(applePrice));
+console.log(typeof orangePrice);
+console.log(typeof applePrice);
+
+var another_applePrice =parseInt('fascist vagse');
+console.log(another_applePrice);
+
+//
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log (total);
+console.log(total.toFixed(2));
+console.log(parseFloat(total.toFixed(2)));;
