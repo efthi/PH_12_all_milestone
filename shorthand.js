103ac516998b97eeedf1d00643135e91adea73f3
Md Eftequarul Alam <efthaqur@gmail.com>
2025-07-28 00:03:23 +0600

    shorhand cleared

---
+++ shorthand.js
@@ -0,0 +1,23 @@
+var price = 35;
+// price = price +10;
+price += 10;
+console.log(price);
+
+var fridge = 35000;
+// fridge = fridge - 1000;
+fridge -= 1000;
+console.log(fridge);
+
+/*
+    Multiply
+*/
+
+var number = 10;
+number *= 10;
+console.log(number); 
+
+//Division
+
+var divide = 200;
+divide /= 10;
+console.log(divide);
